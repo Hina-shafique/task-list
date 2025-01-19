380ffc3a69f7f281b162af2bebace0db2edd59d9
Hina Shafique <hinashafique4159@gmail.com>
2025-01-19 18:02:26 +0500

    withoutvite

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>project 2</title>
-    @vite('resources/css/app.css', 'resources/js/app.js')
+     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     <section class="max-w-4xl mx-auto mt-10 p-5 bg-white container">
